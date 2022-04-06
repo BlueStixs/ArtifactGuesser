10427e86b729a31005d5310f3675ad9230508995
Andrew Wang <57112250+AndrewxWang@users.noreply.github.com>
2022-04-05 20:08:25 -0400

    big changes

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 var completed = [];
-
-var charOne = Math.floor(Math.random() * (artifactList.length - 1));
-var charTwo;
-
 var score = 0;
+var charOne;
 
 function homePage() {
     document.location.reload();
@@ -23,36 +20,46 @@ function loadSite() {
 }
 
 function play() {
+    document.getElementById("logo").style.width = "25vh";
 
     clearAll();
 
-    document.getElementById("logo").style.width = "25vh";
-
     // random characters    
-    if (charTwo == null) {
-        charTwo = generateRandom(charOne);
+    if (charOne == null) {
+        charOne = generateRandom();
         fadeIn();
     } else {
-        charOne = charTwo;
-        charTwo = generateRandom(charOne);
+        charOne = generateRandom();
+        slideIn();
 
-        document.getElementById("image1").style.opacity = 0;
-        slideLeft();
+
+        // document.getElementById("myArtifact").style.opacity = 0;
+        // slideIn();
     }
-    completed.push(charOne);
 
-    makeCharInfoVisible()
-    document.getElementById("image2").style.display = "none";
+
+    setTimeout(function () {
+        document.getElementById("censorBar").style.opacity = 1;
+        setTimeout(function () {
+            document.getElementById("myArtifact").src = "images/artifacts/" + charOne + ".png";
+        }, 100)
+    }, 200)
+
+    setTimeout(function () {
+        document.getElementById("charImage").style.visibility = "visible";
+        document.getElementById("charImage").style.opacity = 1;
+    }, 500);
 
 }
 
-function generateRandom(charOne) {
-    var charTwo = Math.floor(Math.random() * (artifactList.length - 1));
+function generateRandom() {
+    var randomArtifact = Math.floor(Math.random() * (artifactList.length - 1));
 
-    if (charOne == charTwo || completed.includes(charTwo)) {
+    if (completed.includes(randomArtifact)) {
+        console.log("MONKA");
         var monkaW = 0;
-        while (charOne == charTwo || completed.includes(charTwo)) {
-            charTwo = Math.floor(Math.random() * (artifactList.length - 1))
+        while (completed.includes(randomArtifact)) {
+            randomArtifact = Math.floor(Math.random() * (artifactList.length - 1))
             monkaW += 1;
             if (monkaW > 20) { //pretty sure the while loop should work fine but just in case OMEGALUL
                 break;
@@ -60,18 +67,39 @@ function generateRandom(charOne) {
         }
     }
     //adds current character to completed
-    completed.push(charTwo);
+    completed.push(randomArtifact);
 
-    return charTwo;
-}
-
-function addDecimals(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return randomArtifact;
 }
 
 function checkWin(input) {
-    document.getElementById("buttons").style.display = "none";
 
+
+    console.log("Your Input:" + input);
+
+    console.log("Actual:" + artifactList[charOne]);
+
+    setTimeout(function () {
+        document.getElementById("censorBar").style.opacity = 0;
+    }, 300);
+
+    //win
+    if (input == artifactList[charOne]) {
+        setTimeout(function () {
+            score++;
+            changeScore(score);
+            slideUp();
+
+            playerWinLose("win");
+            setTimeout(function () {
+            }, 300);
+            play();
+        }, 2000);
+    } else {
+        setTimeout(function () {
+            playerWinLose("lose");
+        }, 2000);
+    }
 }
 
 function changeScore(score) {
@@ -85,59 +113,48 @@ function changeScore(score) {
 }
 
 function playerWinLose(check) {
+    console.log(check);
     if (score == artifactList.length - 1 || check == "lose") {
         setTimeout(function () {
-            clearAll();
+            document.getElementById("charImage").style.display = "none";
+            document.getElementById("buttons").style.display = "none";
+
             //resets everything
-            charOne = Math.floor(Math.random() * (artifactList.length - 1));
-            charTwo = null;
+            charOne = null;
             completed = [];
             score = 0;
-
             document.getElementById(check).style.display = "block";
             document.getElementById("finalscore").innerHTML = "Your final score: " + score;
             document.getElementById("currScore").innerHTML = "Score: " + score;
-        }, 1000);
+        }, 400);
     } else {
         play();
     }
 }
 
-//If you're still reading this. STOP reading ONWARD 
-
 function clearAll() {
-    document.getElementById("logo").style.display = "none";
     document.getElementById("win").style.display = "none";
     document.getElementById("lose").style.display = "none";
-    document.getElementById("image1").style.display = "none";
-    document.getElementById("image2").style.display = "none";
-    document.getElementById("highscore").style.display = "none";
-    document.getElementById("currScore").style.display = "none";
+    document.getElementById("charImage").style.display = "block";
+    document.getElementById("charImage").style.opacity = 0;
     document.getElementById("homeScreen").style.opacity = 0;
     setTimeout(function () {
         document.getElementById("homeScreen").style.display = "none";
     }, 300);
 }
-function makeCharInfoVisible() {
-    document.getElementById("logo").style.display = "block";
-    document.getElementById("image1").style.display = "block";
-    document.getElementById("censorBar").style.display = "block";
-    document.getElementById("image2").style.display = "block";
-    document.getElementById("highscore").style.display = "block";
-    document.getElementById("currScore").style.display = "block";
-}
+
+
+
+
 
 //animation stuff
 function fadeIn() {
-    document.getElementById("image1").style.opacity = 0;
-    document.getElementById("image2").style.opacity = 0;
-    document.getElementById("image1").src = artifactList[charOne][2];
-    document.getElementById("image2").src = artifactList[charTwo][2];
-    
+    document.getElementById("myArtifact").style.display = "block";
+    document.getElementById("myArtifact").style.opacity = 0;
+
     setTimeout(function () {
-        document.getElementById("image1").style.opacity = 1;
-        document.getElementById("image2").style.opacity = 1;
-        setTimeout(function (){
+        document.getElementById("myArtifact").style.opacity = 1;
+        setTimeout(function () {
 
             document.getElementById("buttons").style.display = "block";
         }, 300);
@@ -151,27 +168,17 @@ function scaleUp(myId) {
     }, 250);
 }
 
-function slideRight() {
-    document.getElementById("image2").style.transform = "translateX(-60.5vh)"
+function slideUp() {
+    document.getElementById("charImage").style.transform = "translateY(-20.5vh)"
 
 }
 
-function slideLeft() {
+function slideIn() {
+    document.getElementById("charImage").style.opacity = "0";
     setTimeout(function () {
-        document.getElementById("image1").style.opacity = 1;
-        document.getElementById("image2").style.opacity = 0;
+        document.getElementById("charImage").style.transform = "translateY(0)";
         setTimeout(function () {
-            document.getElementById("image1").src = artifactList[charOne][2];
-        }, 100);
-        setTimeout(function () {
-            document.getElementById("image2").src = artifactList[charTwo][2];
-            document.getElementById("image2").style.transform = "translateX(0)";
+            document.getElementById("charImage").style.opacity = "1";
         }, 400);
-        setTimeout(function () {
-            document.getElementById("image2").style.opacity = 1;
-            document.getElementById("buttons").style.display = "block";
-        }, 1000);
     }, 400);
-
-
 }
